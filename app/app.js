58256afc7056330f86d689caf16a22c13ba83ff8
bcwaters <dev@AstroAuction.com>
2018-03-12 09:48:04 -0700

    correct app.js static directory issue

--- app/app.js
+++ app/app.js
@@ -3,6 +3,6 @@ const app = express()
 server = require('http').createServer(app)
 var path = require('path')
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => res.sendFile(__dirname +'app/index.html'))
-app.listen(process.env.PORT || 5000,() => console.log('Example app listening on port 3000!'))
+app.use(express.static(path.join(__dirname, '../public')));
+app.get('/', (req, res) => res.sendFile(__dirname +'index.html'))
+app.listen(process.env.PORT || 3000,() => console.log('Example app listening on port 3000!'))
