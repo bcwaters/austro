e21d2341fe3ab328369a256b1332713a00a62965
bcwaters <dev@AstroAuction.com>
2018-03-12 09:36:08 -0700

    Restructure directory for react

--- app/app.js
+++ app/app.js
@@ -3,6 +3,6 @@ const app = express()
 server = require('http').createServer(app)
 var path = require('path')
 
-app.use(express.static(path.join(__dirname, 'resources')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => res.sendFile(__dirname +'/index.html'))
 app.listen(process.env.PORT || 5000,() => console.log('Example app listening on port 3000!'))
