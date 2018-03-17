acbd7f6a840377646a1fc1584e05fd998f5f10ab
bcwaters <dev@AstroAuction.com>
2018-03-17 13:08:42 -0700

    forgot to add cs

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import logo from './logo.png';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import NavComp from './components/NavComp.js';
-import AuctionItem from './components/AuctionItemComp.js'
+import AuctionItem from './components/AuctionItemComp.js';
+import AuctionItemList from './components/AuctionItemList';
 
 export default class App extends Component {
+  
   render() {
     return (
       <div className="App">
@@ -13,8 +15,8 @@ export default class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <div className="App-navbar"> <NavComp /></div>
         </header>
-        <div class="AuctionItemList"><AuctionItem name='Sunrise' bidAmount="$50" imgPath="./SunsetPainting.jpg"/></div>
-        <div class="AuctionItemList"><AuctionItem name='Seasons' bidAmount="$35" imgPath="./Seasons.jpg"/></div>
+        <AuctionItemList><AuctionItem description="Description of a painting goes here" name='Sunrise' bidAmount="$50" imgPath="./SunsetPainting.jpg"/>
+        <AuctionItem name='Seasons' bidAmount="$35" imgPath="./Seasons.jpg"/></AuctionItemList>
       </div>
     
     );
