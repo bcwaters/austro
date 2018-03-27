96076ce617decd83a1cc9eb32c9a371c7b33736b
bcwaters <dev@AstroAuction.com>
2018-03-27 11:02:52 -0700

    asdf

--- src/components/AuctionItemComp.js
+++ src/components/AuctionItemComp.js
@@ -7,16 +7,16 @@ export default class AuctionItemComp extends React.Component {
   setImgStyle(imgSrc) {
     //card size is 180w x 320h
     //img height max is .20*320
+    var MAX_HEIGHT = 150;
+    var MAX_WIDTH = 180;
+    var newImg = new Image();
 	  newImg.src = imgSrc; // this must be done AFTER setting onload
 	  newImg.onload=function(){
-    var MAX_HEIGHT = 150;
-     var MAX_WIDTH = 180;
-    var newImg = new Image();
     
-	console.log(imgSrc )
-	  console.log(newImg)
-	  
-    console.log(newImg.height + " " + newImg.width )
+	  console.log(imgSrc )
+	  console.log(newImg)  
+    console.log(newImg.naturalHeight + " " + newImg.natualWidth )
+	  }
 
 if(newImg.width>newImg.height)
     { //horizontal image so fill width
@@ -24,7 +24,7 @@ if(newImg.width>newImg.height)
       return {
         boxShadow: "3px 3px 3px 3px grey",
         margin: "5px",
-        height: (MAX_WIDTH/newImg.width)*newImg.height,
+        height: (MAX_WIDTH/newImg.naturalWidth)*newImg.naturalHeight,
         width: MAX_WIDTH,
         textAlign: "center"
       };
@@ -35,9 +35,8 @@ if(newImg.width>newImg.height)
         margin: "5px",
         textAlign: "center",
         height: MAX_HEIGHT,
-        width:(MAX_HEIGHT/newImg.height)*newImg.width,
+        width:(MAX_HEIGHT/newImg.naturalHeight)*newImg.naturalWidth,
         };
-	  }
 }
 	render()
 	{
